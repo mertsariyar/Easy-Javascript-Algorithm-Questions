cd2a86a4df1dc902846d9173f1344d56fd3ae01f
Mert <101225081+mertsariyar@users.noreply.github.com>
2024-03-05 06:19:29 +0300

    username validation

---
+++ Javascript Algorithm - Copy/easy/1_userValidation.js
@@ -0,0 +1,35 @@
+export const userValidation = (str) => {
+
+  if(
+    str.length >= 4 && str.length <=25 &&
+    (/[a-zA-Z]/).test(str.slice(0,1)) &&
+    (/^\w+$/).test(str) &&
+    (/[a-zA-Z0-9]/).test(str.slice(str.length -1))
+  ){
+    return true;
+  }else {
+    return false;
+  }
+ 
+
+  //and operator
+  //.lenght property
+  //Reg Exp
+  //.charAt() .slice()
+  //.test();
+  
+  
+
+}
+
+
+/* Rules
+
+1. The username is between 4 and 25 char.
+2. It must start with a letter.
+3. It can only contain letters,numbers and the underscore char.
+4. It cannot end with an underscore char.
+
+if the username is valid then your program should return the string true.
+
+*/
---
+++ Javascript Algorithm - Copy/easy/easy.js
@@ -0,0 +1,7 @@
+import { userValidation } from "./1_userValidation.js";
+
+
+
+
+console.log(userValidation("u__hello_world123"))
+
